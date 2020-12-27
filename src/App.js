b82e5eb6a4330039efd689c22f264c395a53fec9
tmuzanenhamo <tawanda@quickconnectwireless.net>
2020-12-27 12:02:19 +0200

    added routing to our application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage/home-page.component';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage=()=>{
+  return(
+    <div>
+      This is a hats page
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
