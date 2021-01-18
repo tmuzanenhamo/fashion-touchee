0271ea6676ab2f0eaa1ac61503433ab3ab2100fe
tmuzanenhamo <tawanda@quickconnectwireless.net>
2021-01-18 10:59:19 +0200

    Updated the application using reselect and started using memoirs in order to stop other components from rerendering when state not related to them changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 import {connect} from 'react-redux';
 const CartDrpdown =({ cartItems })=>(
     <div className='cart-dropdown'>
@@ -14,8 +15,8 @@ const CartDrpdown =({ cartItems })=>(
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDrpdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,9 @@
+import { createSelector  } from 'reselect';
+
+// input selector
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems );
+
+export const selectCartartItemsCount = createSelector([selectCartItems], (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0) );
